37949e249f8115494a7737f6e7a8754e39fe9a45
jayhenry <zhaopenghao@baixing.net>
2013-10-13 23:52:15 +0800

    修改初始化A的bug。添加注释。

--- Q1.py
+++ Q1.py
@@ -1,66 +1,56 @@
 # -*- coding:utf-8 -*-
 import sys
 from iter_gen import Gospers_Hack
+
 print 'Hello, let\'s start.'
 f = open('tsp.txt')
 n = int(f.readline().split()[0])
 print '# vertices:', n
+# 读入所有点(x,y)到一个列表V，其元素是元组(x,y)
 V = []
 for line in f:
     coordinate = [float(x) for x in line.split() ]
     V.append(tuple(coordinate) )
+# 建立顶点列表V的反向词典Vi,key是某个点(x,y)，value是这个点在V中的索引值index
 Vi = {}
 for i in range(len(V)):
     Vi[V[i]] = i
 print 'Data read.'
-
+# 计算所有顶点之间的距离，即边权值，到一个二维数组C。C[i][j]=Vi到Vj的距离
 print 'Compute edge cost...'
 C = [[0 for j in range(n)] for i in range(n) ]
 for i in range(n):
     for j in range(n):
 	C[i][j] = pow( pow( V[i][0]-V[j][0], 2)+pow( V[i][1]-V[j][1], 2), 0.5)
 
-m = 2
-# S = set()
-i = 0
-A = [[]]
-
 print 'Base case...'
-# SS = []
-# for i in range(pow(2,n-1)):
-#     S = set([0])
-#     vi = 1
-#     while i > 0:
-# 	if i%2 == 1:
-# 	    S.add(vi)
-# 	i >>= 1
-# 	vi += 1
-#     S = frozenset(S)
-#     SS.append(S)
-# print 'size of SS:', len(SS)
-# SSi = {}
-# for i in range(len(SS)):
-#     SSi[SS[i]] = i
-nSS = pow(2,n-1) #用一个25bit的整形数S代表各顶点的子集合S，所以子集合构成的集合SS的元素数为2^(n-1)
-# A = [[float("+inf") for j in range(n) ] for i in range(n)]
+nSS = pow(2,n-1) #用一个n=25bit的整形数S代表顶点集V的子集合S，所以子集合S构成的集合SS的元素数为2^(n-1)
+# A是循环时所用的上一次的(子集S,终点i)对应最短路径的字典。这里S长度为m-1.
+# B是该次循环计算得到的新的(S,i)->最短路径的字典。这里S长度为m.
 A = {}
-for i in range(n):
-    A[i] = [float("+inf") for j in range(n) ]
+# A初始化长度为1的子集
+for S in Gospers_Hack(1,25):
+    A[S] = [float("+inf") for j in range(n) ]
+A[0] = [float("+inf") for j in range(n) ]
 A[0][0] = 0.0
 B = {}
 print 'Recurrence...'
+# 第一层遍历子集的长度，从2到n
 for m in range(2,n+1):
-    B = {}
-    for S in Gospers_Hack(m):
+    # 第二层遍历m长度所有的子集，从0B0..01..1(m个1,25-m个0)到(0B1..10..0)
+    for S in Gospers_Hack(m-1,24):
 	B[S] = [0.0 for i in range(n)]
+	# 第三层遍历所有终点:1...n-1
 	for i in range(1,n):
 	    minV = float("+inf")
+	    # 最优子结构：由m-1长度S计算m长度S对应的最短周游路径
 	    for k in range(1,n):
 		if S&(1<<(k-1)) != 0 and k != i: # 遍历S去掉某个顶点的子集
 	            S_ = S ^ (1<<(k-1))
 		    minV =  min( minV, A[S_][k]+C[k][i] )
 	    B[S][i] = minV
     A = B
+    B = {}
 print 'Solution to the original problem...'
 S = pow(2,n-1) - 1
 # minV = min j=1->n-1 { A[S][j] + C[j][0] }
--- iter_gen.py
+++ iter_gen.py
@@ -1,8 +1,9 @@
+# -*- coding:utf-8 -*-
 class Gospers_Hack:
-    def __init__(self, m ):
-	self.m = m
-	self.first = pow(2, m-1 ) - 1
-	self.last = pow(2, 24 ) - pow(2, 24-m+1 )
+    def __init__(self, m, n ):
+	self.m = m # 比特数
+	self.first = pow(2, m ) - 1
+	self.last = pow(2, n ) - pow(2, n-m )
     def __iter__(self):
 	self.x = self.first
 	return self
@@ -11,9 +12,9 @@ class Gospers_Hack:
 	if (xr > self.last ):
 	    raise StopIteration
 	x = self.x
-        s = x & (-x)
-        r = s + x
-        x = r | (((x ^ r) >> 2) // s)
+        s = x & (-x) # 找到最右边1的位置
+        r = s + x # 找到最右边连续1(k个)的终点位置+1，并将此位置置1
+        x = r | (((x ^ r) >> 2) // s) # 将(k-1)个连续1放到尾部
 	self.x = x
 	return xr
 # gh = Gospers_Hack(2)
